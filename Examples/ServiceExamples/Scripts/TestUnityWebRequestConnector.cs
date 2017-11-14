58728704815256599ab8793fc7c90e7521268d2e
Ajiemar <ajiemar@gmail.com>
2017-11-14 11:57:23 -0600

    testing unity web request

---
+++ Examples/ServiceExamples/Scripts/TestUnityWebRequestConnector.cs
@@ -0,0 +1,132 @@
+﻿using IBM.Watson.DeveloperCloud.Logging;
+using IBM.Watson.DeveloperCloud.Utilities;
+using MiniJSON;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace IBM.Watson.DeveloperCloud.Connection
+{
+    public class TestUnityWebRequestConnector : MonoBehaviour
+    {
+        private string _discoveryUrl = "https://gateway.watsonplatform.net/discovery/api/v1/environments";
+        private string _discoveryUsername = "d1c62431-617d-40a9-b729-5113d2cb34fa";
+        private string _discoveryPassword = "0lcYAxBHkpIL";
+        private string _discoveryVersionDate = "2017-09-01";
+
+        private string _toneAnalyzerUrl = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone";
+        private string _toneAnalyzerUsername = "24766a29-abb6-4273-be74-eb137616b52a";
+        private string _toneAnalyzerPassword = "i0TuJECGdJFQ";
+        private string _toneAnalyzerVersionDate = "2017-09-21";
+        private string _stringToTestTone = "This service enables people to discover and understand, and revise the impact of tone in their content. It uses linguistic analysis to detect and interpret emotional, social, and language cues found in text.";
+
+        private string _visualRecognitionUrl = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers";
+        private string _visualRecognitionApiKey = "visualrecognitionapikeytotestquerieswith";
+        private string _visualRecogntionVersionDate = "2016-05-20";
+
+        Credentials _discoveryCredentials;
+        Credentials _toneAnalyzerCredentials;
+
+        void Start()
+        {
+            LogSystem.InstallDefaultReactors();
+            _discoveryCredentials = new Credentials(_discoveryUsername, _discoveryPassword, _discoveryUrl);
+            _toneAnalyzerCredentials = new Credentials(_toneAnalyzerUsername, _toneAnalyzerPassword, _toneAnalyzerUrl);
+            //Runnable.Run(GetCall());
+            //Runnable.Run(PostCallBody());
+            Runnable.Run(PostCallFormData());
+        }
+
+        private IEnumerator GetCall()
+        {
+            UnityWebRequest wr = UnityWebRequest.Get(_discoveryUrl + "?version=" + _discoveryVersionDate);
+            wr.SetRequestHeader("Authorization", _discoveryCredentials.CreateAuthorization());
+
+            yield return wr.SendWebRequest();
+
+            if (string.IsNullOrEmpty(wr.error))
+            {
+                Log.Debug("TestUnityWebRequestConnector.GetCall()", "Success: {0}", wr.downloadHandler?.text);
+            }
+            else
+            {
+                Log.Error("TestUnityWebRequestConnector.GetCall()", "Error: {0}", wr.error);
+            }
+        }
+
+        private IEnumerator PostCallBody()
+        {
+            Dictionary<string, string> upload = new Dictionary<string, string>();
+            upload["text"] = _stringToTestTone;
+            string json = Json.Serialize(upload);
+            byte[] jsonData = Encoding.UTF8.GetBytes(json);
+
+            UnityWebRequest wr = new UnityWebRequest(_toneAnalyzerUrl + "?version=" + _toneAnalyzerVersionDate);
+            wr.method = UnityWebRequest.kHttpVerbPOST;
+            wr.uploadHandler = new UploadHandlerRaw(jsonData);
+            wr.uploadHandler.contentType = "application/json";
+            wr.downloadHandler = new DownloadHandlerBuffer();
+            wr.SetRequestHeader("Authorization", _toneAnalyzerCredentials.CreateAuthorization());
+
+            yield return wr.SendWebRequest();
+
+            if (wr.isNetworkError || wr.isHttpError)
+            {
+                Log.Error("TestUnityWebRequestConnector.PostCallBody()", "Error: {0}: {1} | {2}", wr.responseCode, wr.error, wr.downloadHandler?.text);
+            }
+            else
+            {
+                Log.Debug("TestUnityWebRequestConnector.PostCallBody()", "Success: {0}", wr.downloadHandler?.text);
+            }
+        }
+
+        private IEnumerator PostCallFormData()
+        {
+            string positiveExamplesPath = Application.dataPath + "/Watson/Examples/ServiceExamples/TestData/visual-recognition-classifiers/turtle_positive_examples.zip";
+            string negativeExamplesPath = Application.dataPath + "/Watson/Examples/ServiceExamples/TestData/visual-recognition-classifiers/negative_examples.zip";
+            byte[] positiveExamplesData = File.ReadAllBytes(positiveExamplesPath);
+            byte[] negativeExamplesData = File.ReadAllBytes(negativeExamplesPath);
+
+            //Dictionary<string, byte[]> positiveExamplesData = new Dictionary<string, byte[]>();
+            //byte[] positiveExamplesData = null;
+            //byte[] negativeExamplesData = null;
+
+
+            //WWWForm form = new WWWForm();
+            //form.AddBinaryData("giraffe_positive_examples", positiveExamplesData, "giraffe_positive_examples.zip", "application/zip");
+            //form.AddBinaryData("negative_examples", negativeExamplesData, "negative_examples.zip", "application/zip");
+
+            List<IMultipartFormSection> multipartForm = new List<IMultipartFormSection>();
+            multipartForm.Add(new MultipartFormDataSection("giraffe_positive_examples", positiveExamplesData, "application/zip"));
+            multipartForm.Add(new MultipartFormDataSection("negative_examples", negativeExamplesData, "application/zip"));
+            //multipartForm.Add(new MultipartFormFileSection("turtle_positive_examples", positiveExamplesData, "turtle_positive_examples.zip", "application/zip"));
+            //multipartForm.Add(new MultipartFormFileSection("negative_examples", negativeExamplesData, "negative_examples.zip", "application/zip"));
+            multipartForm.Add(new MultipartFormDataSection("name", "test-unity-web-request-classifier"));
+
+            //UnityWebRequest wr = UnityWebRequest.Post(_visualRecognitionUrl + "?version=" + _visualRecogntionVersionDate + "&api_key=" + _visualRecognitionApiKey, multipartForm);
+
+            UnityWebRequest wr = new UnityWebRequest(_visualRecognitionUrl + "?version=" + _visualRecogntionVersionDate + "&api_key=" + _visualRecognitionApiKey);
+            byte[] serializedForm = UnityWebRequest.SerializeFormSections(multipartForm, UnityWebRequest.GenerateBoundary());
+            wr.uploadHandler = new UploadHandlerRaw(serializedForm);
+            wr.uploadHandler.contentType = "multipart/form-data";
+            wr.method = UnityWebRequest.kHttpVerbPOST;
+
+            wr.downloadHandler = new DownloadHandlerBuffer();
+            //wr.SetRequestHeader("Content-Type", "multipart/form-data");
+
+            yield return wr.SendWebRequest();
+
+            if (wr.isNetworkError || wr.isHttpError)
+            {
+                Log.Error("TestUnityWebRequestConnector.PostCallFormData()", "Error: {0}: {1} | {2}", wr.responseCode, wr.error, wr.downloadHandler?.text);
+            }
+            else
+            {
+                Log.Debug("TestUnityWebRequestConnector.PostCallFormData()", "Success: {0}", wr.downloadHandler?.text);
+            }
+        }
+    }
+}
---
+++ Scripts/Connection/UnityWebRequestConnector.cs
@@ -0,0 +1,75 @@
+﻿/**
+* Copyright 2015 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+// uncomment to enable debugging
+//#define ENABLE_DEBUGGING
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IBM.Watson.DeveloperCloud.Connection
+{
+    public class UnityWebRequestConnector
+    {
+        public const string AUTHENTICATION_TOKEN_AUTHORIZATION_HEADER = "X-Watson-Authorization-Token";
+        public const string AUTHENTICATION_AUTHORIZATION_HEADER = "Authorization";
+        public const long HTTP_STATUS_OK = 200;
+        public const long HTTP_STATUS_CREATED = 201;
+        public const long HTTP_STATUS_NO_CONTENT = 204;
+
+        #region Public Types
+        /// <summary>
+        /// This delegate type is declared for a Response handler function.
+        /// </summary>
+        /// <param name="req">The original request object.</param>
+        /// <param name="resp">The response object.</param>
+        //public delegate void ResponseEvent(Request req, Response resp);
+
+        /// <summary>
+        /// This delegate is invoked to provide download progress.
+        /// </summary>
+        /// <param name="progress"></param>
+        public delegate void ProgressEvent(float progress);
+        /// <summary>
+        /// The class is returned by a Request object containing the response to a request made
+        /// by the client.
+        /// </summary>
+        public class Response
+        {
+            #region Public Properties
+            /// <summary>
+            /// True if the request was successful.
+            /// </summary>
+            public bool Success { get; set; }
+            /// <summary>
+            /// Error message if Success is false.
+            /// </summary>
+            public string Error { get; set; }
+            /// <summary>
+            /// The data returned by the request.
+            /// </summary>
+            public byte[] Data { get; set; }
+            /// <summary>
+            /// The amount of time in seconds it took to get this response from the server.
+            /// </summary>
+            public float ElapsedTime { get; set; }
+            #endregion
+        };
+#endregion
+    }
+}
